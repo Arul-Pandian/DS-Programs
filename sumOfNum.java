07e1ff37b043186c95bc84662d9a5fbd6e2ec6e0
Arul-Pandian <arul.irullappan@gmail.com>
2024-09-03 09:27:29 +0530

    Create sumOfNum.java

---
+++ sumOfNum.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n=108;
+        int sum=0;
+        int rem;
+        while(n>0){
+            rem=n%10;
+            sum=sum+rem;
+            n=n/10;
+            
+        }
+        System.out.print(sum);
+	}
+}
