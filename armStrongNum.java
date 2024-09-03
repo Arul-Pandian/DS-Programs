d5cf864dfc1a1fb9f297eba6f1169b7e8d9a40d2
Arul-Pandian <arul.irullappan@gmail.com>
2024-09-03 12:24:36 +0530

    Update armStrongNum.java

--- armStrongNum.java
+++ armStrongNum.java
@@ -1,21 +1,26 @@
-
+import java.lang.Math;
 class HelloWorld {
     public static void main(String[] args) {
-        int n=153;
+        int n=407;
         int temp=n;
-        int rem=0;
-        int a=0;
+        int num = temp;
+        int c=0,rem=0;
         while(temp>0){
-            rem=temp%10;
-            a=a+(rem*rem*rem);
+            c++;
             temp=temp/10;
-            
         }
-        if(a==n){
-            System.out.print("Armstrong");
+        int sum=0;
+        while(num >0){
+            rem=num%10;
+            sum=sum+(int)Math.pow((double)rem,(double)c);
+            num=num/10;
+        }
+       if(sum==n){
+            System.out.println("Armstrong");
         }
         else{
-            System.out.print("Not Armstrong");
+            System.out.println("Not an Armstrong");
         }
+        
     }
 }
